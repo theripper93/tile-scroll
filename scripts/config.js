105089a2f557f6f2e37d15daf2c6c4f7e28b022b
theripper93 <tsplab@gmail.com>
2022-09-07 14:56:44 +0200

    a

--- scripts/config.js
+++ scripts/config.js
@@ -1,7 +1,11 @@
 Hooks.on("renderTileConfig", (app, html, data) => {
     const enableScroll = app.object.getFlag("tile-scroll", "enableScroll") ?? false;
+    const enableRotate = app.object.getFlag("tile-scroll", "enableRotate") ?? false;
     const scrollSpeed = app.object.getFlag("tile-scroll", "scrollSpeed") ?? 1;
     const scrollDirection = app.object.getFlag("tile-scroll", "scrollDirection") ?? 0;
+    const repeat = app.object.getFlag("tile-scroll", "repeat") ?? 1;
+    const pivotx = app.object.getFlag("tile-scroll", "pivotx") ?? 0.5;
+    const pivoty = app.object.getFlag("tile-scroll", "pivoty") ?? 0.5;
 
     const injectHtml = `
     <h3 class="form-header"><i class="fas fa-angle-double-right"></i> ${game.i18n.localize("tile-scroll.flags.header")}</h3>
@@ -14,9 +18,9 @@ Hooks.on("renderTileConfig", (app, html, data) => {
     </div>
 
     <div class="form-group">
-        <label for="height">${game.i18n.localize("tile-scroll.flags.scrollSpeed")}</label>
+        <label>${game.i18n.localize("tile-scroll.flags.enableRotate")}</label>
         <div class="form-fields">
-            <input type="number" value="${scrollSpeed}" step="any" name="flags.tile-scroll.scrollSpeed">
+            <input type="checkbox" name="flags.tile-scroll.enableRotate" ${enableRotate ? 'checked=""' : ""}>
         </div>
     </div>
 
@@ -26,6 +30,35 @@ Hooks.on("renderTileConfig", (app, html, data) => {
             <input type="number" value="${scrollDirection}" step="any" name="flags.tile-scroll.scrollDirection">
         </div>
     </div>
+
+    <div class="form-group">
+        <label for="height">${game.i18n.localize("tile-scroll.flags.scrollSpeed")}</label>
+        <div class="form-fields">
+            <input type="number" value="${scrollSpeed}" step="any" name="flags.tile-scroll.scrollSpeed">
+        </div>
+    </div>
+
+    <div class="form-group">
+        <label for="height">${game.i18n.localize("tile-scroll.flags.repeat")}</label>
+        <div class="form-fields">
+            <input type="number" value="${repeat}" step="any" name="flags.tile-scroll.repeat">
+        </div>
+    </div>
+
+    <div class="form-group">
+        <label for="height">${game.i18n.localize("tile-scroll.flags.pivotx")}</label>
+        <div class="form-fields">
+            <input type="number" value="${pivotx}" step="any" name="flags.tile-scroll.pivotx">
+        </div>
+    </div>
+
+    <div class="form-group">
+        <label for="height">${game.i18n.localize("tile-scroll.flags.pivoty")}</label>
+        <div class="form-fields">
+            <input type="number" value="${pivoty}" step="any" name="flags.tile-scroll.pivoty">
+        </div>
+    </div>
+
     `;
     html.find('input[name="video.volume"]').closest(".form-group").after(injectHtml);
 });
--- scripts/main.js
+++ scripts/main.js
@@ -15,10 +15,28 @@ class TileScrollShader extends BaseSamplerShader {
     uniform float tilescroll_time;
     uniform float tilescroll_speed;
     uniform float tilescroll_direction;
+    uniform bool tilescroll_scroll;
+    uniform bool tilescroll_rotate;
+    uniform float tilescroll_repeat;
+    uniform vec2 tilescroll_pivot;
     varying vec2 vUvs;
   
     void main() {
-      vUvs = aTextureCoord + vec2(tilescroll_time * tilescroll_speed * cos(tilescroll_direction), tilescroll_time * tilescroll_speed * sin(tilescroll_direction));
+      vUvs = aTextureCoord * tilescroll_repeat;
+      if(tilescroll_scroll) {
+        vUvs += vec2(tilescroll_time * tilescroll_speed * cos(tilescroll_direction), tilescroll_time * tilescroll_speed * sin(tilescroll_direction));
+      }
+      if(tilescroll_rotate) {
+        vec2 textureRotate_center_uv = tilescroll_pivot;
+        vec2 textureRotate_center = textureRotate_center_uv * tilescroll_repeat;
+        float textureRotate_r = distance(vUvs, textureRotate_center);
+        vec2 textureRotate_vUv = (vUvs - textureRotate_center) / textureRotate_center_uv;
+        float textureRotate_angle = atan(textureRotate_vUv.y, textureRotate_vUv.x);
+        textureRotate_angle = textureRotate_angle + tilescroll_time * tilescroll_speed;
+        vec2 textureRotate_newUv = vec2(cos(textureRotate_angle), sin(textureRotate_angle)) * textureRotate_r;
+        vUvs = (textureRotate_newUv) + textureRotate_center;
+      }
+
       gl_Position = vec4((projectionMatrix * vec3(aVertexPosition, 1.0)).xy, 0.0, 1.0);
     }
   `;
@@ -29,6 +47,10 @@ class TileScrollShader extends BaseSamplerShader {
     tilescroll_speed: 1,
     tilescroll_time: 0,
     tilescroll_direction: 0,
+    tilescroll_scroll: false,
+    tilescroll_rotate: false,
+    tilescroll_repeat: 1,
+    tilescroll_pivot: [0.5, 0.5],
   };
 
   _preRender(mesh) {
@@ -36,11 +58,15 @@ class TileScrollShader extends BaseSamplerShader {
     this.uniforms.tilescroll_time = canvas.app.ticker.lastTime;
     this.uniforms.tilescroll_speed = (this.tile.document.flags["tile-scroll"]?.scrollSpeed ?? 1) / 10000;
     this.uniforms.tilescroll_direction = Math.toRadians(this.tile.document.flags["tile-scroll"]?.scrollDirection ?? 0);
+    this.uniforms.tilescroll_scroll = this.tile.document.flags["tile-scroll"]?.enableScroll ?? false;
+    this.uniforms.tilescroll_rotate = this.tile.document.flags["tile-scroll"]?.enableRotate ?? false;
+    this.uniforms.tilescroll_repeat = this.tile.document.flags["tile-scroll"]?.repeat ?? 1;
+    this.uniforms.tilescroll_pivot = [this.tile.document.flags["tile-scroll"]?.pivotx ?? 0.5, this.tile.document.flags["tile-scroll"]?.pivoty ?? 0.5];
   }
 }
 
 Hooks.on("drawTile", (tile, layer, context) => {
-  if(tile.document.flags["tile-scroll"]?.enableScroll) {
+  if(tile.document.flags["tile-scroll"]?.enableScroll || tile.document.flags["tile-scroll"]?.enableRotate) {
     tile.mesh.setShaderClass(TileScrollShader);
     tile.mesh.texture.baseTexture.wrapMode = PIXI.WRAP_MODES.REPEAT
     tile.mesh.shader.tile = tile;
@@ -51,8 +77,8 @@ Hooks.on("drawTile", (tile, layer, context) => {
 
 Hooks.on("updateTile", (tile, updates) => {
   if(!tile.object) return;
-  if(updates.flags?.["tile-scroll"]?.enableScroll !== undefined) {
-    tile.object.mesh.setShaderClass(updates.flags["tile-scroll"].enableScroll ? TileScrollShader : BaseSamplerShader);
+  if(updates.flags?.["tile-scroll"] !== undefined) {
+    tile.object.mesh.setShaderClass(tile.flags["tile-scroll"].enableScroll || tile.flags["tile-scroll"].enableRotate ? TileScrollShader : BaseSamplerShader);
     tile.object.mesh.texture.baseTexture.wrapMode = PIXI.WRAP_MODES.REPEAT
     tile.object.mesh.texture.baseTexture.update()
     tile.object.mesh.shader.tile = tile.object;
